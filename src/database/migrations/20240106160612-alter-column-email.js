810486bcb3dc155dbb36dffadf35d70f97882fa6
3DUTEX <t3xdev@gmail.com>
2024-01-06 13:09:16 -0300

    campo email alterado para unique

---
+++ src/database/migrations/20240106160612-alter-column-email.js
@@ -0,0 +1,18 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn(
+      'Users',
+      'email',
+      {
+        type: Sequelize.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
