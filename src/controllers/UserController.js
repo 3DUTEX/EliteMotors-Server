3c664207927d0beb76f6f2dfc8e3637d7ba484ee
3DUTEX <t3xdev@gmail.com>
2024-01-06 13:18:48 -0300

    create user feito

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,26 +1,40 @@
 import User from '../models/User';
 
+function error400(res, type, message) {
+  return res.status(400).json({
+    error: {
+      type,
+      message,
+    },
+  });
+}
+
 export const create = async (req, res) => {
   try {
     const {
       id, name, email, isGoogleAccount,
     } = await User.create(req.body);
 
-    res.status(201).json({
+    const user = {
       id,
       name,
       email,
       isGoogleAccount,
+    };
 
-    });
+    return res.status(201).json(user);
   } catch (e) {
-    const { errors } = e;
-    const error = Array.isArray(errors) ? errors[0] : errors;
-    res.status(400).json({
-      error: {
-        type: error.type,
-        message: error.message,
-      },
-    });
+    if (e.errors) {
+      const { errors } = e;
+      // Se for um array pega apenas o indíce 0
+      const error = Array.isArray(errors) ? errors[0] : errors;
+      return error400(res, error.type, error.message);
+    }
+
+    const error = {
+      type: 'Unexpected error',
+      message: 'Please contact developer of system',
+    };
+    return error400(res, error.type, error.message);
   }
 };
