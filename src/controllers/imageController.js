1ef159edcaf89a15efde606460a3037c51e4ffbe
3DUTEX <t3xdev@gmail.com>
2024-01-12 09:34:20 -0300

    migration table images feita

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,5 +1,6 @@
 import randomNumber from '../helpers/randomNumber';
 import supabase from '../config/supabase';
+import { errorCatch } from './errors/error400';
 
 const BUCKET = 'Elite Motors';
 
@@ -8,16 +9,17 @@ export const create = async (req, res) => {
     const { file } = req; // File from request (multer)
     const { mimetype } = file; // Image Type
 
+    const expiresIn = 7776000;
     const numberID = randomNumber(); // Name of image
 
     // Upload Image
     await supabase.storage.from(BUCKET).upload(`cars/${numberID}`, file.buffer, { contentType: mimetype });
 
     // Generate URL
-    const { data } = await supabase.storage.from(BUCKET).createSignedUrl(`cars/${numberID}`, 120);
+    const { data } = await supabase.storage.from(BUCKET).createSignedUrl(`cars/${numberID}`, expiresIn);
 
     return res.status(201).json(data);
   } catch (e) {
-    console.log(e);
+    return errorCatch(res, e);
   }
 };
---
+++ src/database/migrations/20240112122135-create-table-images.js
@@ -0,0 +1,33 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'Images',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true,
+        },
+        storageID: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        url: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        vehicleID: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+      },
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Images');
+  },
+};
