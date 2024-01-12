18d9811b8a9e3140c1937db90e5eaabf1b95de0a
3DUTEX <t3xdev@gmail.com>
2024-01-12 14:30:13 -0300

    migration create table reservations

---
+++ src/database/migrations/20240112172431-create-table-reservations.js
@@ -0,0 +1,44 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'Reservations',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: false,
+          autoIncrement: true,
+        },
+        vehicleID: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Vehicles',
+            key: 'id',
+          },
+        },
+        userID: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+        },
+        visitDate: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        createdBy: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+      },
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reservations');
+  },
+};
