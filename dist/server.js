aebcbb2eacac3c6b031f72b645d98d4c02456346
3DUTEX <t3xdev@gmail.com>
2024-01-13 12:17:22 -0300

    cors configurado

--- dist/server.js
+++ dist/server.js
@@ -3,6 +3,7 @@
 // Import dependecies
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 
 // Import modules
 var _useRoutes = require('./helpers/useRoutes'); var _useRoutes2 = _interopRequireDefault(_useRoutes);
@@ -11,6 +12,7 @@ var _routes = require('./routes'); var _routes2 = _interopRequireDefault(_routes
 _dotenv2.default.config();
 const app = _express2.default.call(void 0, );
 
+app.use(_cors2.default.call(void 0, ));
 app.use(_express2.default.json()); // Configurando json no express
 
 const useRoutes = new (0, _useRoutes2.default)(app);
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@
 // Import dependecies
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 // Import modules
 import UseRoutes from './helpers/useRoutes';
@@ -11,6 +12,7 @@ import routes from './routes';
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // Configurando json no express
 
 const useRoutes = new UseRoutes(app);
