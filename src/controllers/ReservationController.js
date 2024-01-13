6ea3d5f2b670025fd77c74779b771fe269701838
3DUTEX <t3xdev@gmail.com>
2024-01-13 11:09:35 -0300

    CRUD reservations feito

--- src/controllers/ReservationController.js
+++ src/controllers/ReservationController.js
@@ -1,12 +1,29 @@
-import { format } from 'date-fns';
 import Reservation from '../models/Reservation';
-import { errorCatch } from './errors/error400';
+import { error400, errorCatch } from './errors/error400';
+
+function createError(type, message) {
+  return {
+    error: {
+      type,
+      message,
+    },
+  };
+}
+
+async function findReservation(req) {
+  const reservation = await Reservation.findByPk(req.params.id);
+  if (!reservation) return createError('bad request', 'reservation is not finded');
+  if (reservation.userID !== req.userId) {
+    return createError('Unauthorized', 'this reservation does not belong to the logged in user');
+  }
+  return reservation;
+}
 
 export const create = async (req, res) => {
   try {
     const reservationData = {
       userID: req.userId,
-      vehicleID: req.params.vehicleID,
+      vehicleID: req.body.vehicleID,
       visitDate: req.body.visitDate || '',
       createdBy: new Date(),
     };
@@ -16,6 +33,63 @@ export const create = async (req, res) => {
     return res.status(201).json(reservation);
   } catch (e) {
     console.log(e);
+    return errorCatch(res, e);
+  }
+};
+
+export const show = async (req, res) => {
+  const reservation = await findReservation(req);
+  if (reservation.error) {
+    const { type, message } = reservation.error;
+    if (type === 'Unauthorized') return res.status(401).json(reservation);
+    return error400(res, type, message);
+  }
+
+  return res.status(200).json(reservation);
+};
+
+export const index = async (req, res) => {
+  try {
+    const reservations = await Reservation.findAll({ where: { userID: req.userId } });
+    if (!reservations) {
+      return error400(res, 'bad request', 'none reservation finded');
+    }
+    return res.status(200).json(reservations);
+  } catch (e) {
+    return errorCatch(res, e);
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const reservation = await findReservation(req);
+    if (reservation.error) {
+      const { type, message } = reservation.error;
+      if (type === 'Unauthorized') return res.status(401).json(reservation);
+      return error400(res, type, message);
+    }
+
+    const reservationUpdated = await reservation.update(req.body);
+
+    return res.status(200).json(reservationUpdated);
+  } catch (e) {
+    return errorCatch(res, e);
+  }
+};
+
+export const deleteReservation = async (req, res) => {
+  try {
+    const reservation = await findReservation(req);
+    if (reservation.error) {
+      const { type, message } = reservation.error;
+      if (type === 'Unauthorized') return res.status(401).json(reservation);
+      return error400(res, type, message);
+    }
+
+    await reservation.destroy();
+
+    return res.status(204).json();
+  } catch (e) {
     return errorCatch(res, e);
   }
 };
--- src/routes/reservationRoute.js
+++ src/routes/reservationRoute.js
@@ -1,11 +1,24 @@
 import { Router } from 'express';
 
+import isAdmin from '../middlewares/isAdmin';
 import authMiddleware from '../middlewares/authMiddleware';
 import * as ReservationController from '../controllers/ReservationController';
 
 const router = new Router();
 
 // POST
-router.post('/reservations/:vehicleID', authMiddleware, ReservationController.create);
+router.post('/reservations', authMiddleware, ReservationController.create);
+
+// GET
+router.get('/reservations', authMiddleware, ReservationController.index);
+
+// GET
+router.get('/reservations/:id', authMiddleware, ReservationController.show);
+
+// PUT
+router.put('/reservations/:id', authMiddleware, ReservationController.update);
+
+// DELETE
+router.delete('/reservations/:id', authMiddleware, ReservationController.deleteReservation);
 
 export default router;
